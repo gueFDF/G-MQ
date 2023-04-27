0a8da22fc075bbac9811459dd9946613177f0423
gueFDF <3237455241@qq.com>
2023-04-27 09:36:03 +0800

    添加写模式

--- abstract/protocol.go
+++ abstract/protocol.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Client interface {
-	Close()
+	Close() error
 }
 
 type Protocol interface {
@@ -15,7 +15,8 @@ type Protocol interface {
 	IOLOOP(Client) error
 }
 
-// 写消息，处理黏包
+// 写函数
+// 加入长度前缀
 func SendResponse(w io.Writer, data []byte) (int, error) {
 	err := binary.Write(w, binary.BigEndian, int32(len(data)))
 	if err != nil {
@@ -28,4 +29,23 @@ func SendResponse(w io.Writer, data []byte) (int, error) {
 	}
 
 	return (n + 4), nil
+}
+
+// 写函数
+// 加入长度前缀和帧头
+func SendFramedResponse(w io.Writer, Type int32, data []byte) (int, error) {
+	err := binary.Write(w, binary.BigEndian, int32(len(data)+4))
+	if err != nil {
+		return 0, err
+	}
+	err = binary.Write(w, binary.BigEndian, (uint32)(Type))
+	if err != nil {
+		return 4, err
+	}
+	n, err := w.Write(data)
+	if err != nil {
+		return n + 8, err
+	}
+
+	return n + 8, err
 }
